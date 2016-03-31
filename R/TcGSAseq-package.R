3442917909905d595728c8c68ee21220da043269
denisagniel <denis.agniel@mail.harvard.edu>
2016-03-31 10:36:16 -0400

    Initial commit

---
+++ R/TcGSAseq-package.R
@@ -0,0 +1,16 @@
+#' TcGSAseq: a package to perform Time-course Gene Set Analysis of RNA-seq data
+#' 
+#' Gene set analysis of longitudinal RNA-seq data with varaince component score 
+#' test acocunting for data heteroscedasticity through precision weights.
+#' 
+#' The main functions of the TcGSAseq package is \code{\link{tcgsa_seq}}
+#' 
+#' @docType package
+#' 
+#' @references Agniel D, Hejblum BP, Variance component score test for 
+#' time-course gene set analysis of longitudinal RNA-seq data, \emph{submitted}, 2016. 
+#' 
+#' @name TcGSAseq
+#' @importFrom GSA GSA.read.gmt
+#' 
+NULL
