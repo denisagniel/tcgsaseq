afc1247bfa2f7cd8cf042a902630269f09165124
borishejblum <bhejblum.hsph@gmail.com>
2016-04-24 18:50:10 -0400

    Ready for CRAN submission

--- R/TcGSAseq-package.R
+++ R/TcGSAseq-package.R
@@ -1,16 +1,27 @@
-#' TcGSAseq: a package to perform Time-course Gene Set Analysis of RNA-seq data
+#'TcGSAseq: a package to perform Time-course Gene Set Analysis of RNA-seq data
 #'
-#' Gene set analysis of longitudinal RNA-seq data with varaince component score
-#' test acocunting for data heteroscedasticity through precision weights.
+#'Gene set analysis of longitudinal RNA-seq data with varaince component score
+#'test acocunting for data heteroscedasticity through precision weights.
 #'
-#' The main functions of the TcGSAseq package is \code{\link{tcgsa_seq}}
+#'\tabular{ll}{
+#'Package: \tab tcgsaseq\cr
+#'Type: \tab Package\cr
+#'Version: \tab 1.0.0\cr
+#'Date: \tab 2016-04-28\cr
+#'License:\tab \href{http://www.gnu.org/licenses/gpl-2.0.txt}{GPL-2}\cr
+#'}
+#'The main functions of the TcGSAseq package is \code{\link{tcgsa_seq}}
 #'
-#' @docType package
 #'
-#' @references Agniel D, Hejblum BP, Variance component score test for
-#' time-course gene set analysis of longitudinal RNA-seq data, \emph{submitted}, 2016.
+#'@author Boris P. Hejblum, Denis Agniel
+#'--- Maintainer: Boris P. Hejblum
 #'
-#' @name tcgsaseq
-#' @importFrom GSA GSA.read.gmt
+#'@references Agniel D, Hejblum BP, Variance component score test for
+#'time-course gene set analysis of longitudinal RNA-seq data, \emph{submitted}, 2016.
+#'
+#'
+#'@docType package
+#'@name tcgsaseq
+#'@importFrom GSA GSA.read.gmt
 #'
 NULL
