658a2757c780dfac429e001af884ba141117d5bc
Boris <boris.hejblum.ensai@gmail.com>
2017-02-19 17:28:48 +0100

    !! genewise permutation problem still: no low pvalues

--- R/vc_test_perm.R
+++ R/vc_test_perm.R
@@ -112,7 +112,7 @@ vc_test_perm <- function(y, x, indiv=rep(1,nrow(x)), phi, w, Sigma_xi = diag(nco
                                              Sigma_xi = Sigma_xi)$gene_scores_unscaled
     }
     browser()
-    pvals2 <- 1-rowMeans(sapply(gene_scores_perm, function(x){x<gene_scores_obs}))
+    pvals <- 1-rowMeans(sapply(gene_scores_perm, function(x){x<gene_scores_obs}))
     ans <- list("gene_scores_obs" = gene_scores_obs, "gene_pvals" = pvals)
   }else{
     scores_perm <- numeric(n_perm)
