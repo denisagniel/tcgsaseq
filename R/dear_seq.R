1b45a86bbd0cb00ab122f14f2340287ff14f5edf
Mgauth <37074876+Mgauth@users.noreply.github.com>
2020-03-10 10:10:58 +0100

    Add comments to the example code

--- R/dear_seq.R
+++ R/dear_seq.R
@@ -164,34 +164,49 @@
 #'[DOI: 10.1101/635714v1](https://www.biorxiv.org/content/10.1101/635714v1).
 #'
 #'@examples
-#'nsims <- 2 #100
+#number of runs
+#'nsims <- 2 #100 
 #'res <- numeric(nsims)
+
 #'for(i in 1:nsims){
-#'n <- 1000
-#'nr=5
-#'ni=50
-#'r <- nr*ni
-#'t <- matrix(rep(1:nr), ni, ncol=1, nrow=r)
-#'sigma <- 0.5
-#'b0 <- 1
-#'
-#'#under the null:
-#'b1 <- 0
-#'y.tilde <- b0 + b1*t + rnorm(r, sd = sigma)
-#'y <- t(matrix(rnorm(n*r, sd = sqrt(sigma*abs(y.tilde))), ncol=n, nrow=r) +
-#'       matrix(rep(y.tilde, n), ncol=n, nrow=r))
-#'x <- matrix(1, ncol=1, nrow=r)
-#'
-#'#run test
-#'res_genes <- dear_seq(exprmat=y, covariates=x, variables2test=t,
-#'                    sample_group=rep(1:ni, each=nr),
-#'                    which_test='asymptotic',
-#'                    which_weights='none', preprocessed=TRUE)
-#'mean(res_genes$pvals[, 'rawPval']>0.05)
-#'quantile(res_genes$pvals[, 'rawPval'])
-#'res[i] <- mean(res_genes$pvals[, 'rawPval']<0.05)
-#'message(i)
+#'  n <- 1000 #number of genes
+#'  nr=5 #number of measurements per subject (grouped data)
+#'  ni=50 #number of subjects
+#'  r <- nr*ni #number of measurements
+#'  t <- matrix(rep(1:nr), ni, ncol=1, nrow=r) # the variable to be tested 
+#'  sigma <- 0.5
+#'  b0 <- 1
+#'  
+#'  #under the null:
+#'  b1 <- 0
+#'  
+#'  #create the matrix of gene expression
+#'  y.tilde <- b0 + b1*t + rnorm(r, sd = sigma)
+#'  y <- t(matrix(rnorm(n*r, sd = sqrt(sigma*abs(y.tilde))), ncol=n, nrow=r) +
+#'           matrix(rep(y.tilde, n), ncol=n, nrow=r))
+#'  
+#'  #no covariates
+#'  x <- matrix(1, ncol=1, nrow=r)
+#'  
+#'  #run test
+#'  #asymptotic test with preprocessed grouped data
+#'  res_genes <- dear_seq(exprmat=y, covariates=x, variables2test=t,
+#'                        sample_group=rep(1:ni, each=nr),
+#'                        which_test='asymptotic',
+#'                       which_weights='none', preprocessed=TRUE)
+#' 
+#'  #proportion of raw p-values>0.05
+#'  mean(res_genes$pvals[, 'rawPval']>0.05)
+#'  
+#'  #quantiles of raw p-values
+#'  quantile(res_genes$pvals[, 'rawPval'])
+#'  
+#'  #proportion of raw p-values<0.05 i.e. proportion of DE genes
+#'  res[i] <- mean(res_genes$pvals[, 'rawPval']<0.05)
+#'  message(i)
 #'}
+#'
+#'#Monte-Carlo estimation of the proportion of DE genes over n simulations
 #'mean(res)
 #'
 #'if(interactive()){
